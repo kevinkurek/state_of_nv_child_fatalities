111f04c1a774ab9ed0fb80a0fb629ac9acd084b9
kevinkurek <kevin@theinfinityfund.com>
2023-07-11 13:00:29 -0700

    mock tests, restructure dict test

--- tests/test_download.py
+++ tests/test_download.py
@@ -3,7 +3,7 @@ import pytest
 from unittest.mock import MagicMock, patch
 import requests
 
-from ..scripts.child_fatality_scrape import download_all_pdfs, merge_dicts
+from ..scripts.child_fatality_scrape import download_all_pdfs, list_files, restructure_alphabetical_dict, merge_dicts
 
 
 @pytest.fixture
@@ -48,6 +48,22 @@ def test_download_all_pdfs(mock_html_page, mock_pdf_content):
             assert mock_get.call_args_list[1][0][0] == 'https://dcfs.nv.gov//uploadedFiles/dcfsnvgov/content/Programs/CWS/CPS/ChildFatalities/Clark/document1.pdf'  # Second request is for the first PDF
             assert mock_get.call_args_list[2][0][0] == 'https://dcfs.nv.gov//uploadedFiles/dcfsnvgov/content/Programs/CWS/CPS/ChildFatalities/Clark/document2.pdf'  # Third request is for the second PDF
 
+def test_list_files(tmp_path):
+    # tmp_path is a pytest fixture that provides a temporary directory unique to this test invocation
+    dummy_files = ["file1.txt", "file2.txt"]
+    for file_name in dummy_files:
+        # Create dummy files in the temporary directory
+        (tmp_path / file_name).touch()
+
+    assert set(list_files(tmp_path)) == set(dummy_files)  # Test that the list_files function correctly lists out the files
+
+
+def test_restructure_alphabetical_dict():
+
+    input_dict = {'A': 'Date of the notification to the child welfare agency of the fatality/near fatality of a child:\n4/13/2023\n', 'B': 'Location of child at the time of death or near fatality (city/county):\nLas Vegas, Clark\n'}
+    expected_output_dict = {'Date of the notification to the child welfare agency of the fatality/near fatality of a child': '4/13/2023', 'Location of child at the time of death or near fatality (city/county)': 'Las Vegas, Clark'}
+
+    assert restructure_alphabetical_dict(input_dict) == expected_output_dict
 
 def test_merge_dicts():
     # Given
